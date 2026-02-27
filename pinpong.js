86eaeb0c2d2bda6ba8634dfeb790a41a78003cf9
Jennifenayanne <mello.jennifer@escola.pr.gov.br>
2026-02-27 07:55:46 -0300

    upde

--- pinpong.js
+++ pinpong.js
@@ -17,15 +17,44 @@ let xRaqueteOponente = 585;
 let yRaqueteOponente = 150;
 
 //Placa
-let meusPontos = 0;
-let pontosOponente = 0;
+let meusPontos = 0
+let pontosOponente = 0
+
+//dificuldade
+
+let dificuldade = "Normal";
+let trilha,ponto,raquetada;
+
+function preload(){
+  trilha = loadSound(" trilha.mp3")
+  ponto = loadSound("ponto.mp3")
+  raquete = loadSound("raquete.mp3")
+}
 
 function setup ( ){
     createCarnas(600,400)
+    trilha.loop()
 }
 
 function draw(){
+   background(0);
+   movimentoBolinha();
+   raquetes(xRaquete,yRaquete);
+   raquetes(xRaqueteOponente,
+           yRaqueteOponente);
+   npc(dificuldade);
+
+  if(keyIsDown(UP_ARROW)){
+    yRaquete -=  10}
   
+  if(keyIsDown(DOWN_ARROW)){
+    yRaquete +=  10}
+  
+  if(xBolinha -raio < xRaquete + 10 &
+     yBolinha-raio< yRaquete + 90 &
+     yBolinha +raio > yRaquete){
+    velocidadeXBolinha *= -1;
+    raquetada.play()}
 }
 
 function movimentoBoloinha(){
